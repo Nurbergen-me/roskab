8b1be68a8540b30c191a1da83afb316736d95fa6
nurik.eleshov <nurik.eleshov@gmail.com>
2023-06-30 10:32:41 +0800

    slider update

--- js/main.js
+++ js/main.js
@@ -17,6 +17,21 @@ burgerClose.addEventListener('click', function() {
 
 
 // Initialize Swiper 
+var swiper = new Swiper(".heroSwiper", {
+    initialSlide: 0,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+        dynamicBullets: true,
+        dynamicMainBullets: 5,
+    },
+});
+
+// Initialize Swiper 
 var swiper = new Swiper(".mySwiper", {
     initialSlide: 0,
     navigation: {
