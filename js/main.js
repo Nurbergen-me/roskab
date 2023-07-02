54a1f0fa4a960d4adbf996f1194e2d35f171d018
nurik.eleshov <nurik.eleshov@gmail.com>
2023-07-02 23:29:06 +0800

    pages

--- js/main.js
+++ js/main.js
@@ -5,46 +5,132 @@ const hidden = document.querySelector('.hidden')
 const burgerMenu = document.querySelector('.header_top_mobile_burger')
 const burgerClose = document.querySelector('#burger_close')
 
-burgerMenu.addEventListener('click', function() {
-    hidden.classList.add('show')
-    body.classList.add('noscroll')
-})
+if (burgerMenu) {
+    burgerMenu.addEventListener('click', function() {
+        hidden.classList.add('show')
+        body.classList.add('noscroll')
+    })
+    
+    burgerClose.addEventListener('click', function() {
+        hidden.classList.remove('show')
+        body.classList.remove('noscroll')
+    })
+}
 
-burgerClose.addEventListener('click', function() {
-    hidden.classList.remove('show')
-    body.classList.remove('noscroll')
-})
-
-
-// Initialize Swiper 
-var swiper = new Swiper(".heroSwiper", {
-    initialSlide: 0,
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-    pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-        dynamicBullets: true,
-        dynamicMainBullets: 5,
-    },
-});
 
 // Initialize Swiper 
-var swiper = new Swiper(".mySwiper", {
-    initialSlide: 0,
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-    pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-        dynamicBullets: true,
-        dynamicMainBullets: 5,
-        renderBullet: function (index, className) {
-        return '<span class="' + className + '">' + (index + 1999) + "</span>";
+const heroSwiper = document.querySelector('.heroSwiper')
+if (heroSwiper) {
+    var swiper = new Swiper(".heroSwiper", {
+        initialSlide: 0,
+        navigation: {
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
         },
-    },
-});
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+            dynamicBullets: true,
+            dynamicMainBullets: 5,
+        },
+    });
+}
+
+// Initialize Swiper 
+const mySwiper = document.querySelector('.mySwiper')
+if (mySwiper) {
+    var swiper = new Swiper(".mySwiper", {
+        initialSlide: 0,
+        navigation: {
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
+        },
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+            dynamicBullets: true,
+            dynamicMainBullets: 5,
+            renderBullet: function (index, className) {
+            return '<span class="' + className + '">' + (index + 1999) + "</span>";
+            },
+        },
+    });
+    
+}
+
+
+// Initialize Swiper About
+const aboutSwiper = document.querySelector('.aboutSwiper')
+if (aboutSwiper) {
+    var swiper = new Swiper(".aboutSwiper", {
+        slidesPerView: 2,
+        spaceBetween: 18,
+        initialSlide: 1,
+        navigation: {
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
+        },
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+            dynamicBullets: true,
+            dynamicMainBullets: 5,
+        },
+        breakpoints: {
+            320: {
+                slidesPerView: 1,
+            },
+            768: {
+                slidesPerView: 2,
+            },
+            992: {
+                slidesPerView: 3,
+            }
+        }
+    });
+}
+
+
+// Vacancies
+
+const vacanciesList = document.querySelectorAll('.vacancies_item_top')
+
+if (vacanciesList) {
+    vacanciesList.forEach(item => {
+        item.addEventListener('click', (e) => {
+            vacanciesList.forEach(elem => {
+                elem.parentNode.classList.remove('active')
+            })
+            item.parentNode.classList.add('active')
+        })
+    })
+}
+
+
+// Sidebar
+
+const sidebarMenu = document.querySelectorAll('.sidebar_item_top')
+
+if (sidebarMenu) {
+    sidebarMenu.forEach(item => {
+        item.addEventListener('click', (e) => {
+            sidebarMenu.forEach(elem => {
+                elem.parentNode.classList.remove('active')
+            })
+            item.parentNode.classList.add('active')
+        })
+    })
+}
+
+const sidebarSubMenu = document.querySelectorAll('.sidebar_item_item_top')
+
+if (sidebarSubMenu) {
+    sidebarSubMenu.forEach(item => {
+        item.addEventListener('click', (e) => {
+            sidebarSubMenu.forEach(elem => {
+                elem.parentNode.classList.remove('active')
+            })
+            item.parentNode.classList.add('active')
+        })
+    })
+}
